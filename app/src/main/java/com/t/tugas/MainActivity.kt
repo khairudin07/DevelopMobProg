60f3f94cd8a46f6d337ef8262c6b348138b20afb
khairudin07 <48312011+khairudin07@users.noreply.github.com>
2020-03-27 20:22:59 +0700

    Add files via upload

---
+++ app/src/main/java/com/t/tugas/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.t.tugas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnHitung.setOnClickListener {
+            val tahunLahir = editInputUmur.text.toString()
+            val tahun: Int = Calendar.getInstance().get(Calendar.YEAR)
+
+            var umur = 0
+            if (tahunLahir.toIntOrNull() !=null) {
+                umur = tahun - tahunLahir.toInt()
+                txtUmur.text = "Umur = $umur Tahun"
+            }else{
+                txtUmur.text = "Tahun Tidak Valid"
+            }
+        }
+    }
+}
